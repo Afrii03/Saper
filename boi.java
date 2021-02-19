7d8450379a96a15eb5c13eda99ce18c9e34b9e22
Afri <79155979+Afrii03@users.noreply.github.com>
2021-02-19 18:17:45 +0100

    Add files via upload

---
+++ boi.java
@@ -0,0 +1,11 @@
+  import javax.swing.JFrame;
+public class boi extends JFrame{
+ private static final long serialVersionUID = 1L;
+ public boi(){
+     super("Kolko i krzyzyk");
+     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+     setSize(300,400);
+     setLocation(700,100);
+     setVisible(true);
+ }
+  }
